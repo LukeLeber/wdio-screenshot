c616132151de23b002d1394d3706275ed4d845bd
Luke Leber <lal65@psu.edu>
2019-04-06 13:15:23 -0400

    ...

--- src/modules/makeDocumentScreenshot.js
+++ src/modules/makeDocumentScreenshot.js
@@ -15,17 +15,18 @@ export default async function makeDocumentScreenshot(browser, options = {}) {
 
   // hide scrollbars, scroll to start, hide & remove elements, wait for render
   await beforeScreenshot(browser, options);
-
+  debug('22');
   // get screen dimisions to determine document height & width
   const screenDimensions = (await browser.execute(getScreenDimensions));
+  debug('23');
   const screenDimension = new ScreenDimension(screenDimensions, browser);
-
+  debug('24');
   // make screenshot of area
   const base64Image = await makeAreaScreenshot(browser, 0, 0, screenDimension.getDocumentWidth(), screenDimension.getDocumentHeight());
-
+  debug('25');
   // show scrollbars, show & add elements
   await afterScreenshot(browser, options);
-
+  debug('26');
   log('end document screenshot');
 
   return base64Image;
