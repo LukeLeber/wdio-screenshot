71769b0d1e1799703557d7bdabd894e0c701c852
Luke Leber <lal65@psu.edu>
2019-04-09 13:44:15 -0400

    ...

--- src/scripts/getBoundingRects.js
+++ src/scripts/getBoundingRects.js
@@ -1,13 +1,22 @@
+"use strict";
 
+import debug from "debug";
 
-export default function getBoundingRect(elems) {
-  return elems.map((elem) => {
-    const boundingRect = elem.getBoundingClientRect();
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = getBoundingRect;
+const log = debug('wdio-screenshot:makeElementScreenshot');
+
+function getBoundingRect(elems) {
+  log('getBoundingRect');
+  return elems.map(function (elem) {
+    var boundingRect = elem.getBoundingClientRect();
     return {
       top: boundingRect.top,
       right: boundingRect.right,
       bottom: boundingRect.bottom,
-      left: boundingRect.left,
+      left: boundingRect.left
     };
   });
 }
